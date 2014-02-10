9d70f7bbe1a88ac61c571a0647522ce0541433f9
Nikita Frolov <nikita@frolov.fi>
2014-02-10 20:23:45 +0200

    extraced superclass AbstractCipher

---
+++ src/main/java/tiralabra/aes/core/AbstractCipher.java
@@ -0,0 +1,82 @@
+package tiralabra.aes.core;
+
+public abstract class AbstractCipher {
+
+    private static final KeyScheduler keyScheduler = new KeyScheduler();
+
+    private final int[] roundKeys;
+
+    public AbstractCipher(final byte[] key) {
+        roundKeys = keyScheduler.schedule(key);
+    }
+
+    /**
+     * Replace each byte of the state with appropriate value from the specified S-box.
+     *
+     * @param   state   state to process
+     * @param   sbox    sbox used to replace
+     */
+    protected void subBytes(final byte[] state, final byte[] sbox) {
+        for (int i = 0; i < state.length; ++i) {
+            state[i] = sbox[state[i] & 0xff];
+        }
+    }
+
+    /**
+     * AddRoundKey() function described in the standard.
+     *
+     * @param   state   state to process
+     * @param   round   round number
+     */
+    protected void addRoundKey(final byte[] state, final int round) {
+        final int roundKeyOffset = round * 4;
+
+        for (int c = 0; c < 4; ++c) {
+            int column = packWord(state, 4 * c);
+            column ^= roundKeys[roundKeyOffset + c];
+            unpackWord(column, state, 4 * c);
+        }
+    }
+
+    protected static int gmul(final byte a, final int b) {
+        return gmul(a & 0xff, b);
+    }
+
+    /**
+     * Performs multiplication in Rijndael Galois field.
+     * Source of inspiration: http://www.samiam.org/galois.html
+     *
+     * @param   a       first factor
+     * @param   b       second factor
+     * @return          product
+     */
+    protected static int gmul(int a, int b) {
+        int product = 0;
+
+        for (int i = 0; i < 8; ++i) {
+            if ((b & 1) == 1) {
+                product ^= a;
+            }
+            final int hi_bit_set = (a & 0x80);
+            a <<= 1;
+            if (hi_bit_set == 0x80) {
+                a ^= 0x1b;
+            }
+            b >>= 1;
+        }
+
+        return product;
+    }
+
+    private static int packWord(final byte[] bs, final int offset) {
+        return (bs[offset] & 0xff) | ((bs[offset + 1] & 0xff) << 8) | ((bs[offset + 2] & 0xff) << 16) | (bs[offset + 3] << 24);
+    }
+
+    private static void unpackWord(final int word, final byte[] bs, final int offset) {
+        bs[offset] = (byte) (word & 0xff);
+        bs[offset + 1] = (byte) ((word >> 8) & 0xff);
+        bs[offset + 2] = (byte) ((word >> 16) & 0xff);
+        bs[offset + 3] = (byte) ((word >> 24) & 0xff);
+    }
+
+}
--- src/main/java/tiralabra/aes/core/Cipher.java
+++ src/main/java/tiralabra/aes/core/Cipher.java
@@ -7,18 +7,13 @@ import java.util.Arrays;
  *
  * @author Nikita Frolov
  */
-public class Cipher {
-
-    private static final KeyScheduler keyScheduler = new KeyScheduler();
-    private static final byte[] sbox = SBox.FORWARD;
-
-    private final int[] roundKeys;
+public class Cipher extends AbstractCipher {
 
     /**
      * @param   key     cipher key
      */
     public Cipher(final byte[] key) {
-        this.roundKeys = keyScheduler.schedule(key);
+        super(key);
     }
 
     /**
@@ -51,30 +46,12 @@ public class Cipher {
     }
 
     /**
-     * AddRoundKey() function described in the standard.
-     *
-     * @param   state   state to process
-     * @param   round   round number
-     */
-    private void addRoundKey(final byte[] state, final int round) {
-        final int roundKeyOffset = round * 4;
-
-        for (int c = 0; c < 4; ++c) {
-            int column = packWord(state, 4 * c);
-            column ^= roundKeys[roundKeyOffset + c];
-            unpackWord(column, state, 4 * c);
-        }
-    }
-
-    /**
      * SubBytes() function as described in the standard.
      *
      * @param   state   state to process
      */
     private void subBytes(final byte[] state) {
-        for (int i = 0; i < state.length; ++i) {
-            state[i] = sbox[state[i] & 0xff];
-        }
+        subBytes(state, SBox.FORWARD);
     }
 
     /**
@@ -121,47 +98,6 @@ public class Cipher {
         }
 
         System.arraycopy(product, 0, state, 0, product.length);
-    }
-
-    private static int gmul(final byte a, final int b) {
-        return gmul(a & 0xff, b);
-    }
-
-    /**
-     * Performs multiplication in Rijndael Galois field.
-     * Source of inspiration: http://www.samiam.org/galois.html
-     *
-     * @param   a       first factor
-     * @param   b       second factor
-     * @return          product
-     */
-    private static int gmul(int a, int b) {
-        int product = 0;
-
-        for (int i = 0; i < 8; ++i) {
-            if ((b & 1) == 1) {
-                product ^= a;
-            }
-            final int hi_bit_set = (a & 0x80);
-            a <<= 1;
-            if (hi_bit_set == 0x80) {
-                a ^= 0x1b;
-            }
-            b >>= 1;
-        }
-
-        return product;
-    }
-
-    private static int packWord(final byte[] bs, final int offset) {
-        return (bs[offset] & 0xff) | ((bs[offset + 1] & 0xff) << 8) | ((bs[offset + 2] & 0xff) << 16) | (bs[offset + 3] << 24);
-    }
-
-    private static void unpackWord(final int word, final byte[] bs, final int offset) {
-        bs[offset] = (byte) (word & 0xff);
-        bs[offset + 1] = (byte) ((word >> 8) & 0xff);
-        bs[offset + 2] = (byte) ((word >> 16) & 0xff);
-        bs[offset + 3] = (byte) ((word >> 24) & 0xff);
     }
 
 }
